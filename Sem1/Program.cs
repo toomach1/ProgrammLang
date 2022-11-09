4f1c89658f4063374a35a1d1411f967cf2364062
Artem <temazasport@mail.ru>
2022-11-09 10:32:35 +0800

    s1z1

--- Sem1/Program.cs
+++ Sem1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 2 числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sqrt=num2*num2;
+if (sqrt==num1){
+    Console.WriteLine("первое число является квадратом второго");
+}
+else{
+    Console.WriteLine("первое число не является квадратом второго");
+}
