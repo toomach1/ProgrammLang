cf34adb5cade7cb6fe7db59aa0249420921a9e22
Artem <temazasport@mail.ru>
2022-11-09 13:34:19 +0800

    s1z4

---
+++ Sem1.3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<0){
+    Console.WriteLine("Число не может быть отрицательным");
+    Console.WriteLine("Введите новое число");
+    int num1 = Convert.ToInt32(Console.ReadLine());  
+    num=num1;
+}
+
+
+
+else{
+    
+    int num_minus=-num;
+while(num_minus!=num){
+    Console.Write(num_minus + ", ");
+    num_minus++;
+}
+}
+
+Console.WriteLine(num);
---
+++ Sem1.4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>99 && num<1000){
+    Console.WriteLine(num%10);
+}
+else{
+    Console.WriteLine("Введено неверное число");
+}
